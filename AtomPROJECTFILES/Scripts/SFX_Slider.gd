43b62cee2426e548f14eb9bc4b2247338d8d112a
Marin Boban <marinhoboban@gmail.com>
2023-06-18 18:05:42 +0200

    reset

--- AtomPROJECTFILES/Scripts/SFX_Slider.gd
+++
@@ -1,9 +0,0 @@
-extends VSlider
-
-func _ready():
-	self.value=$"/root/Global".sfxvol+50;
-
-func _on_SFX_Slider_value_changed(value):
-	$"/root/Global".sfxvol=value-50;
-	if(value==0):
-		$"/root/Global".sfxvol=-100;
--- AtomPROJECTFILES/Scripts/VolumeSlider.gd
+++
@@ -1,9 +0,0 @@
-extends VSlider
-
-func _ready():
-	self.value=$"/root/Global".musicvol+50;
-
-func _on_VolumeSlider_value_changed(value):
-	$"/root/Global".musicvol=value-50;
-	if(value==0):
-		$"/root/Global".musicvol=-100;
